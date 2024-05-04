1a3bfef8fd3bb6e9147e8389428b0c242aae6c96
Kyle <klim385@aucklanduni.ac.nz>
2024-05-04 21:09:29 +1200

    play method displays current number of round

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -5,13 +5,17 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
+  int gameCount = 0;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
-  public void play() {}
+  public void play() {
+    gameCount++;
+    MessageCli.START_ROUND.printMessage(String.valueOf(gameCount));
+  }
 
   public void endGame() {}
 
